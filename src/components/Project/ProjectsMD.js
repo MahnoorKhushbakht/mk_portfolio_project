f10db974b747541e1baa2348795c9a83fafa03c2
mahnoor <mahnoor.bscs3429@iiu.edu.pl>
2024-06-09 19:26:54 +0500

    updated

--- src/components/Project/ProjectsMD.js
+++ src/components/Project/ProjectsMD.js
@@ -6,6 +6,7 @@ import Button from '@mui/material/Button';
 import 'aos/dist/aos.css';
 import project1 from '../images/project1.jpeg'
 import project2 from '../images/project2.jpeg';
+import project3 from '../images/project3.JPG';
 import { Carousel } from 'react-bootstrap';
 
 
@@ -18,12 +19,20 @@ export default function ProjectMD() {
     window.open('https://neskomedia.onrender.com/', '_blank');
   };
 
+  const handleTGMarketingClick = () => {
+    window.open('https://mens-clothing.vercel.app/', '_blank');
+  };
+
   const handleGitPortfolioClick = () => {
     window.open('https://github.com/MahnoorKhushbakht/pe_portfolio_website', '_blank');
   };
 
   const handleGitMarketingClick = () => {
     window.open('https://github.com/MahnoorKhushbakht/marketing-agency', '_blank');
+  };
+
+  const handleGitTGClick = () => {
+    window.open('https://github.com/MahnoorKhushbakht/mens-clothing.git', '_blank');
   };
 
   return (
@@ -102,6 +111,44 @@ export default function ProjectMD() {
                                     }}
                                     size='large'
                                     variant="contained"  onClick={handleGitMarketingClick}>Learn More</Button>
+                </CardActions>
+              </div>
+            </Card>
+</Carousel.Item>
+<Carousel.Item>
+            <Card data-aos="fade-down" data-aos-delay='150' sx={{maxWidth: '60%', backgroundColor: '#ebc521', margin: 'auto', position: 'relative' }}>
+              <CardMedia
+                component="img"
+                alt="project2"
+                height="240"
+                image={project3}
+              />
+              <div style={{ backgroundColor: 'rgba(0, 0, 0, 0.5)', position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                <CardActions style={{ zIndex: 1 }}>
+                  <Button                                 sx={{
+                                  
+                                     background:'#593ea3',
+                                      color: 'white',
+                                      '&:hover': {
+                                        background: 'white',
+                                        color: '#593ea3',
+                                      },
+                                      fontFamily: 'Helvetica, Arial',
+                                    }}
+                                    size='large'
+                                    variant="contained"  onClick={handleTGMarketingClick}>View</Button>
+                  <Button                                 sx={{
+                                  
+                                     background:'#593ea3',
+                                      color: 'white',
+                                      '&:hover': {
+                                        background: 'white',
+                                        color: '#593ea3',
+                                      },
+                                      fontFamily: 'Helvetica, Arial',
+                                    }}
+                                    size='large'
+                                    variant="contained"  onClick={handleGitTGClick}>Learn More</Button>
                 </CardActions>
               </div>
             </Card>
