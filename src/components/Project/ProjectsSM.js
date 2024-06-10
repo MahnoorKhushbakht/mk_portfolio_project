16bee57896634af31bd5893eec155bf243aa383d
mahnoor <mahnoor.bscs3429@iiu.edu.pl>
2024-06-10 21:05:38 +0500

    updated

--- src/components/Project/ProjectsSM.js
+++ src/components/Project/ProjectsSM.js
@@ -3,11 +3,10 @@ import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
-import Box from '@mui/material/Box';
 import 'aos/dist/aos.css';
 import project1 from '../images/project1.jpeg';
 import project2 from '../images/project2.jpeg';
-import Typography from '@mui/material/Typography';
+import project3 from '../images/project3.JPG';
 
 export default function ProjectSM() {
   const handlePortfolioClick = () => {
@@ -18,6 +17,11 @@ export default function ProjectSM() {
     window.open('https://neskomedia.onrender.com/', '_blank');
   };
 
+  const handleTGMarketingClick = () => {
+    window.open('https://mens-clothing.vercel.app/', '_blank');
+  };
+
+
   const handleGitPortfolioClick = () => {
     window.open('https://github.com/MahnoorKhushbakht/pe_portfolio_website', '_blank');
   };
@@ -25,6 +29,10 @@ export default function ProjectSM() {
   const handleGitMarketingClick = () => {
     window.open('https://github.com/MahnoorKhushbakht/marketing-agency', '_blank');
   };
+  const handleGitTGClick = () => {
+    window.open('https://github.com/MahnoorKhushbakht/mens-clothing.git', '_blank');
+  };
+
 
   return (
     <>
@@ -100,6 +108,42 @@ export default function ProjectSM() {
                                     }}
                                     size='small'
                                     variant="contained"  onClick={handleGitMarketingClick}>Github</Button>
+                </CardActions>
+              </div>
+            </Card>
+            <Card data-aos="fade-down" data-aos-delay='150' sx={{maxWidth: '100%',maxHeight: '100%', backgroundColor: '#ebc521', margin: '20px auto', position: 'relative' }}>
+              <CardMedia
+                component="img"
+                alt="project2"
+                height="240"
+                image={project3}
+              />
+              <div style={{ backgroundColor: 'rgba(0, 0, 0, 0.5)', position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                <CardActions style={{ zIndex: 1 }}>
+                  <Button                                 sx={{
+                                  
+                                     background:'#593ea3',
+                                      color: 'white',
+                                      '&:hover': {
+                                        background: 'white',
+                                        color: '#593ea3',
+                                      },
+                                      fontFamily: 'Helvetica, Arial',
+                                    }}
+                                    size='small'
+                                    variant="contained"  onClick={handleTGMarketingClick}>View</Button>
+                  <Button                                 sx={{
+                                  
+                                     background:'#593ea3',
+                                      color: 'white',
+                                      '&:hover': {
+                                        background: 'white',
+                                        color: '#593ea3',
+                                      },
+                                      fontFamily: 'Helvetica, Arial',
+                                    }}
+                                    size='small'
+                                    variant="contained"  onClick={handleGitTGClick}>Github</Button>
                 </CardActions>
               </div>
             </Card>
